173915083b3045f3f270fe8bd04ed8956018a49c
Aryan Singh <as5374329@gmail.com>
2025-12-19 15:06:55 +0530

    fix the tailwind issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: 'es2015',
-    minify: false,
-    rollupOptions: {
-      treeshake: false,
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
 })
